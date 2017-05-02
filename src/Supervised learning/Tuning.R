fed24bcc48bdac0648d755e28994505ae9c20bf2
Barbara Fusinska <barbara.fusinska@gmail.com>
2017-05-02 22:46:20 +0100

    Changed the variable name for tuning

--- src/Supervised learning/Tuning.R
+++ src/Supervised learning/Tuning.R
@@ -9,17 +9,17 @@ getModelInfo('knn')
 
 # tune by 10 the parameters (tuneLength)
 modelLookup('knn')
-tune.fit <- train(labels~., data=myData, trControl=tc.cv, method="knn", tuneLength=10)
+tune.fit <- train(labels~., data=data.class, trControl=tc.cv, method="knn", tuneLength=10)
 tune.fit
 
 # tune by values
 values <- data.frame(k=c(11, 12, 18))
-tune.fit <- train(labels~., data=myData, trControl=tc.cv, method="knn", tuneGrid=values)
+tune.fit <- train(labels~., data=data.class, trControl=tc.cv, method="knn", tuneGrid=values)
 tune.fit
 
 modelLookup('nb')
 values <- data.frame(fL=c(0, 0, 1, 1), 
                      usekernel=c(TRUE, FALSE, TRUE, FALSE), 
                      adjust=c(1, 1, 1, 1))
-tune.fit <- train(labels~., data=myData, trControl=tc.cv, method="nb", tuneGrid=values)
+tune.fit <- train(labels~., data=data.class, trControl=tc.cv, method="nb", tuneGrid=values)
 tune.fit
