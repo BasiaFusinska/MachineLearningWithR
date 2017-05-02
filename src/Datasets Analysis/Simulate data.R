a109e29e2279ad6bbc80c1b64f8715db580f7bee
Barbara Fusinska <barbara.fusinska@gmail.com>
2017-05-02 22:21:18 +0100

    Changed the code of classification simulation to take into account all the features

--- src/Datasets Analysis/Simulate data.R
+++ src/Datasets Analysis/Simulate data.R
@@ -6,15 +6,16 @@ classificationSample <- function(features=2, n=100){
   
   # Generate data, normal distribution
   for(i in 1:features){
-    fname <- paste("f", i, sep = '')
+    fname <- paste("x", i, sep = '')
     data.names <- c(data.names, fname)
     data <- cbind(data, rnorm(n, 0, 5))
   }
   data <- data.frame(data)
   names(data) <- data.names
-  
-  data$labels <- factor(ifelse(data$f1 + data$f2 < 0, 'A', 'B'))
-  
+
+  f.sums <- rowSums(data)
+  data$labels <- factor(ifelse(f.sums < 0, 'A', 'B'))
+
   # Introduce some variation
   idxs <- sample(1:100, 10)
   data$labels[idxs] <- ifelse(data$labels[idxs] == 1, 'B', 'A')
