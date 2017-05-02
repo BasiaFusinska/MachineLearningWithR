6e079779289c4d589c89406924e5b32946fa76fd
Barbara Fusinska <barbara.fusinska@gmail.com>
2017-05-02 22:44:26 +0100

    Changed the variable name for data sampling

--- src/Supervised learning/Sampling.R
+++ src/Supervised learning/Sampling.R
@@ -2,16 +2,16 @@ library(caret)
 library(pROC)
 
 # By default bootstraping is chosen
-boot.fit <- train(labels~., data=myData, method="knn")
+boot.fit <- train(labels~., data=data.class, method="knn")
 boot.fit
 
 # k-fold cross validation (k = 10 by default)
 tc.cv <- trainControl(method="cv")
-cv.fit <- train(labels~., data=myData, trControl=tc.cv, method="knn")
+cv.fit <- train(labels~., data=data.class, trControl=tc.cv, method="knn")
 cv.fit
 
 # Repeated k-fold cross validation
 # Changing the metric from default Accuracy to ROC
 tc.rcv <- trainControl(method="repeatedcv", repeats=3, classProbs = TRUE, summaryFunction = twoClassSummary)
-rcv.fit <- train(labels~., data=myData, trControl=tc.rcv, method="knn", metric="ROC")
+rcv.fit <- train(labels~., data=data.class, trControl=tc.rcv, method="knn", metric="ROC")
 rcv.fit
