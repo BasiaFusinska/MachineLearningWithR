f4e26e6d9129a462cbf2375a71fb3e261a2e2697
Barbara Fusinska <barbara.fusinska@gmail.com>
2017-05-03 11:43:07 +0100

    Added code for EDA for clustering data

---
+++ src/Datasets Analysis/EDA - Clustering.R
@@ -0,0 +1,16 @@
+# Get the data for classification problem
+data.clu <- clusteringSample()
+
+# Explore the data
+dim(data.clu)
+names(data.clu)
+
+class(data.clu$x1)
+class(data.clu$x2)
+
+summary(data.clu)
+
+# Visualise the data
+library(ggplot2)
+
+ggplot(data.clu, aes(x1, x2)) + geom_point()
