9d821df0396981e2219774cd931b2df6220a447c
Barbara Fusinska <barbara.fusinska@gmail.com>
2017-05-04 22:48:57 +0100

    Removed regression sampling and repeated cross validation sampling

--- src/Supervised learning/Sampling.R
+++ src/Supervised learning/Sampling.R
@@ -1,6 +1,10 @@
 library(caret)
 library(pROC)
 
+# Get the available models for the train function
+names(getModelInfo())
+getModelInfo('knn')
+
 # By default bootstraping is chosen
 boot.fit <- train(labels~., data=data.class, method="knn")
 boot.fit
@@ -10,12 +14,7 @@ tc.cv <- trainControl(method="cv")
 cv.fit <- train(labels~., data=data.class, trControl=tc.cv, method="knn")
 cv.fit
 
-# training for regression
-cv.fit <- train(y~., data=data.reg, trControl=tc.cv, method="lm")
-cv.fit
-
-# Repeated k-fold cross validation
 # Changing the metric from default Accuracy to ROC
-tc.rcv <- trainControl(method="repeatedcv", repeats=3, classProbs = TRUE, summaryFunction = twoClassSummary)
+tc.rcv <- trainControl(method="cv", classProbs = TRUE, summaryFunction = twoClassSummary)
 rcv.fit <- train(labels~., data=data.class, trControl=tc.rcv, method="knn", metric="ROC")
 rcv.fit
