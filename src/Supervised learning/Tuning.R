5511a45534b6bfb37dbff9ca5b036a928a5ee5a9
Barbara Fusinska <barbara.fusinska@gmail.com>
2017-05-04 22:49:56 +0100

    Removed tuning for naive bayes

--- src/Supervised learning/Tuning.R
+++ src/Supervised learning/Tuning.R
@@ -3,10 +3,6 @@ library(caret)
 # Choose the train control as 10-fold cross validation
 tc.cv <- trainControl(method="cv")
 
-# Get the available models for the train function
-names(getModelInfo())
-getModelInfo('knn')
-
 # tune by 10 the parameters (tuneLength)
 modelLookup('knn')
 tune.fit <- train(labels~., data=data.class, trControl=tc.cv, method="knn", tuneLength=10)
@@ -15,11 +11,4 @@ tune.fit
 # tune by values
 values <- data.frame(k=c(11, 12, 18))
 tune.fit <- train(labels~., data=data.class, trControl=tc.cv, method="knn", tuneGrid=values)
-tune.fit
-
-modelLookup('nb')
-values <- data.frame(fL=c(0, 0, 1, 1), 
-                     usekernel=c(TRUE, FALSE, TRUE, FALSE), 
-                     adjust=c(1, 1, 1, 1))
-tune.fit <- train(labels~., data=data.class, trControl=tc.cv, method="nb", tuneGrid=values)
 tune.fit
