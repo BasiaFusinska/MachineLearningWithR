3af3a5c2e7e8c8d10b0f8ff4a806b8d79f888259
Barbara Fusinska <barbara.fusinska@gmail.com>
2017-05-06 23:11:49 +0100

    Updated tasks for Iris classification

--- src/Tasks/Iris Classification.R
+++ src/Tasks/Iris Classification.R
@@ -1,8 +1,9 @@
 # Once you know your data you could start a task of classifying them.
-# You would be using two algorithms: k-Nearest Neighbours and Logistic Regression
+# You would be using two algorithms: k-Nearest Neighbours and Naive Bayes
 # To do this you need to perform few tasks:
 # - Prepare the data if needed (hint: some algorithms need the data to be normalised before beeing fed to the training phase, otherwise some of the dimensions can overtake)
 # - Split the data to train and test (75% in the training set)
-# - Perform training using both k-Nearest Neighbours and Logistic Regression (don't worry about parameters tuning for now)
+# - Perform training using both algorithms (don't worry about parameters tuning for now)
 # - Check the performance of both classificator on the training and test sets
 # - Visualise the classification results and underlying truth
+
